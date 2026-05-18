a1acd6c6b422c5cb280bb792d1b62ecd509594f3
DGuyHub <guydarvin@gmail.com>
2026-05-18 21:47:34 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+# from calc import add
+# from calc import pi
+
+# print(add(100, 200))
+# print(pi)
+
+# import calc 
+# print(calc.add(100, 200))
+
+# print(calc.pi)
+
+from my_math import calc
+
+print(calc.add(100, 200))
+print(calc.pi)
+
+from my_math.calc import pi 
+print(pi)
+
